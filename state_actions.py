6b8ab7414787d600ba086db2e4c43a290f38367a
Шапошников Александр Александрович <AAShaposhnikov.SBT@sberbank.ru>
2018-03-27 15:51:38 +0300

    state update model

---
+++ state_actions.py
@@ -0,0 +1,13 @@
+
+class StateUpdate(object):
+    def __init__(self, log, commitIndex, lastApplied):
+        self.log = log
+        self.commitIndex = commitIndex
+        self.lastApplied = lastApplied
+
+    def perform(self, socket, state):
+        state.append(self.log)
+        if self.commitIndex:
+            state.commitIndex = self.commitIndex
+        if self.lastApplied:
+            state.lastApplied = self.lastApplied
